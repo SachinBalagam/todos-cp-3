49ae078cc98535b0e0511b248a15dccaea72aded
Sachin Balagam <balagamsachin337@gmail.com>
2024-04-08 07:08:45 +0000

    Intial commit

---
+++ src/main/java/com/example/todo/model/Todo.java
@@ -0,0 +1,48 @@
+// Write your code here
+package com.example.todo.model;
+
+public class Todo {
+    private int id;
+    private String todo;
+    private String status;
+    private String priority;
+
+    public Todo(int id, String todo, String status, String priority) {
+        this.id = id;
+        this.todo = todo;
+        this.status = status;
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getTodo() {
+        return this.todo;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public String getPriority() {
+        return this.priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
